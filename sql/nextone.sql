37b4a2b54f771c46dfee3b07ac6ef9168a1700d7
xuyuanfeng <986771570@qq.com>
2019-06-30 23:13:35 +0800

    [update]

--- sql/nextone.sql
+++ sql/nextone.sql
@@ -1,5 +1,5 @@
 /*
- Navicat Premium Data Transfer
+ Navicat MySQL Data Transfer
 
  Source Server         : 本地
  Source Server Type    : MySQL
@@ -11,7 +11,7 @@
  Target Server Version : 50726
  File Encoding         : 65001
 
- Date: 08/05/2019 18:00:16
+ Date: 30/06/2019 23:12:29
 */
 
 SET NAMES utf8mb4;
@@ -45,11 +45,14 @@ COMMIT;
 DROP TABLE IF EXISTS `t_adminusers`;
 CREATE TABLE `t_adminusers` (
   `id` bigint(22) NOT NULL AUTO_INCREMENT,
-  `username` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
-  `salt` varchar(50) NOT NULL,
-  `isenable` tinyint(4) DEFAULT '0',
-  `last_login` datetime DEFAULT NULL,
+  `username` varchar(50) NOT NULL COMMENT '登陆名称',
+  `password` varchar(50) NOT NULL COMMENT '密码',
+  `salt` varchar(50) NOT NULL COMMENT '盐',
+  `isenable` tinyint(4) DEFAULT '0' COMMENT '是否启用',
+  `last_login` datetime DEFAULT NULL COMMENT '最后一次登陆时间',
+  `phone` varchar(50) DEFAULT NULL COMMENT '电话',
+  `nickname` varchar(50) DEFAULT NULL COMMENT '用户名称',
+  `createTime` datetime DEFAULT NULL COMMENT '创建时间',
   PRIMARY KEY (`id`),
   UNIQUE KEY `account` (`username`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
@@ -58,8 +61,8 @@ CREATE TABLE `t_adminusers` (
 -- Records of t_adminusers
 -- ----------------------------
 BEGIN;
-INSERT INTO `t_adminusers` VALUES (1, 'admin', 'da4799f536c020929a55f2631039118f', '111111', 0, '2019-05-08 17:31:44');
-INSERT INTO `t_adminusers` VALUES (2, 'dandan', 'da4799f536c020929a55f2631039118f', '111111', 0, '2019-05-08 17:31:47');
+INSERT INTO `t_adminusers` VALUES (1, 'admin', 'da4799f536c020929a55f2631039118f', '111111', 0, '2019-05-08 17:31:44', '12345678', '小徐', '2019-06-30 20:13:37');
+INSERT INTO `t_adminusers` VALUES (2, 'dandan', 'da4799f536c020929a55f2631039118f', '111111', 0, '2019-05-08 17:31:47', '1234567', '蛋蛋', '2019-06-30 20:13:40');
 COMMIT;
 
 -- ----------------------------
@@ -78,10 +81,10 @@ CREATE TABLE `t_permissions` (
 -- Records of t_permissions
 -- ----------------------------
 BEGIN;
-INSERT INTO `t_permissions` VALUES (1, '/test/list.do', '查看', 0);
-INSERT INTO `t_permissions` VALUES (2, '/test/add.do', '添加', 0);
-INSERT INTO `t_permissions` VALUES (3, '/test/update.do', '修改', 0);
-INSERT INTO `t_permissions` VALUES (4, '/test/delete.do', '删除', 0);
+INSERT INTO `t_permissions` VALUES (1, '/test/list', '查看', 0);
+INSERT INTO `t_permissions` VALUES (2, '/test/add', '添加', 0);
+INSERT INTO `t_permissions` VALUES (3, '/test/update', '修改', 0);
+INSERT INTO `t_permissions` VALUES (4, '/test/delete', '删除', 0);
 COMMIT;
 
 -- ----------------------------
@@ -128,24 +131,27 @@ INSERT INTO `t_roles` VALUES (2, '普通管理员', '基本操作', 0);
 COMMIT;
 
 -- ----------------------------
--- Table structure for t_user
+-- Table structure for t_syslog
 -- ----------------------------
-DROP TABLE IF EXISTS `t_user`;
-CREATE TABLE `t_user` (
-  `id` int(22) NOT NULL,
-  `name` varchar(50) DEFAULT NULL,
-  `username` varchar(50) DEFAULT NULL,
-  `password` varchar(50) DEFAULT NULL,
-  `status` tinyint(4) DEFAULT NULL,
-  `last_login` datetime DEFAULT NULL,
+DROP TABLE IF EXISTS `t_syslog`;
+CREATE TABLE `t_syslog` (
+  `id` int(255) NOT NULL AUTO_INCREMENT COMMENT '日志编号',
+  `name` varchar(255) DEFAULT NULL COMMENT '操作人',
+  `host` varchar(255) DEFAULT NULL COMMENT '主机',
+  `address` varchar(255) DEFAULT NULL COMMENT '操作地点',
+  `createTime` datetime DEFAULT NULL COMMENT '操作时间',
+  `requestPath` varchar(1000) DEFAULT NULL COMMENT '请求地址',
+  `requestParam` varchar(1000) DEFAULT NULL COMMENT '请求参数',
+  `requestMethod` varchar(1000) DEFAULT NULL COMMENT '请求方法',
+  `status` tinyint(1) DEFAULT NULL COMMENT '状态',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of t_user
+-- Records of t_syslog
 -- ----------------------------
 BEGIN;
-INSERT INTO `t_user` VALUES (1, NULL, 'admin', 'admin', NULL, NULL);
+INSERT INTO `t_syslog` VALUES (1, 'admin', '127.0.0.1', ' 测试', '2019-06-30 22:10:42', '../index', '', NULL, 0);
 COMMIT;
 
 SET FOREIGN_KEY_CHECKS = 1;
